552cc8d5def33d3a13e95c9d5792014436ee7543
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-03-14 05:35:40 +0530

    start linkedlist

---
+++ linkedList.cpp
@@ -0,0 +1,29 @@
+#include <iostrem>
+
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node * next;
+
+    Node(){
+        this->data=NULL;
+        this->next=NULL
+    }
+
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+}
+
+class LinkedList(){
+    Node * head;
+    LinkedList(){
+        this->head=NULL;
+    }
+}
+
+
+
