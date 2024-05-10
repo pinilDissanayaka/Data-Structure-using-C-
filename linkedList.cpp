4b133246be98002361cc268c8d18ee7c36d046a6
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-05-10 19:59:15 +0530

    Node class eka haduwa

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,21 +1,24 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
 
 class Node{
-    public:
-        int data;
-        Node * next;
+public:
+    int data;
+    Node * next;
 
-        Node(){
-            this->next=NULL;
-        }
+    Node(){
+        this->data=0;
+        this->next=nullptr;
+    }
 
-        Node(int data){
-            this->data=data;
-            this->next=NULL;
-        }
+    Node(int data){
+        this->data=data;
+        this->next=nullptr;
+    }
 };
 
 
-
+int main(){
+    cout << "Hello";
+    return 0;
+}
